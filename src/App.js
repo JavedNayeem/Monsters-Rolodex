7afa11c65f0f35fb84f54e18de4eba7e8ef0d087
Javed Nayeem <javednayeemavi@gmail.com>
2025-02-05 23:44:04 +0600

    Lesson 32: Set State Callback

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Asif Ahmed'
+      name: {firstName: 'Javed', lastName: 'Nayeem'}
     };
   }
 
@@ -18,9 +18,18 @@ class App extends Component {
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Code By {this.state.name}</p>
+        <p>Hi {this.state.name.firstName} {this.state.name.lastName}</p>
         <button onClick={()=>
-          this.setState({ name: 'Javed Nayeem'})
+          this.setState(
+            () => {
+              return {
+                name: {firstName: 'Farhana', lastName: 'Parveen'},
+              }
+            },
+            () => {
+              console.log(this.state.name);
+            },
+          )
         }>Change Name</button>
       </header>
     </div>
